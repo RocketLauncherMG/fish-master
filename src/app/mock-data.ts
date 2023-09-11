ae36d309f82ec2c941a61b8d3ec813c8aa625512
Rocket Launcher <mdongarza@yahoo.com>
2023-09-11 07:35:21 -0700

    styles update: pictures, fonts, font-sizes

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -5,13 +5,13 @@ export const FISH: Fish[] = [
     {
       name: 'Goldfish',
       species: 'Carassius auratus',
-      image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Faquadiction.world%2Fimg%2Fprofile%2Flionhead-goldfish-8.webp&tbnid=aTB2o2OI0uGv-M&vet=12ahUKEwjd15j0paGBAxVnOjQIHYJ_B5oQMygbegUIARD6AQ..i&imgrefurl=https%3A%2F%2Faquadiction.world%2Fspecies-spotlight%2Flionhead-goldfish%2F&docid=XnLfvJ2u8-juiM&w=2000&h=1330&q=gold%20fish%20image&ved=2ahUKEwjd15j0paGBAxVnOjQIHYJ_B5oQMygbegUIARD6AQ',
+      image: 'assets/gold-fish.jpg',
       description: 'A small freshwater fish known for its bright coloration.',
     },
     {
       name: 'Clownfish',
       species: 'Amphiprioninae',
-      image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.aquariumofpacific.org%2Fimages%2Fmade_new%2Fimages-uploads-clownfish_600_q85.jpg&tbnid=kxTjDZGjIj3ChM&vet=12ahUKEwiiqp2OpqGBAxUUFzQIHQ2LDmcQMygAegUIARCcAQ..i&imgrefurl=https%3A%2F%2Fwww.aquariumofpacific.org%2Fonlinelearningcenter%2Fspecies%2Fclown_anemonefish&docid=BSs_3TxF4mXl1M&w=600&h=450&q=clown%20fish%20image&ved=2ahUKEwiiqp2OpqGBAxUUFzQIHQ2LDmcQMygAegUIARCcAQ',
+      image: 'assets/clown-fish.jpg',
       description: 'A small, colorful fish often found in coral reefs.',
     },
     // Add more fish data here
